d72f002a2cf9c6941509495b787739aea58b3b1c
Sayonara <sayonara.crestani@gmail.com>
2022-12-13 11:25:04 -0300

    return status and message

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,29 @@
+#!/usr/bin/env node
 import chalk from 'chalk';
 import fs from 'fs';
 import { get } from 'http';
 import getFile from './index.js';
+import validList from './http-validacao.js';
 
 const path = process.argv;
 
-function printList(result, id = '') {
-    console.log(
-        chalk.yellow('lista de links'), 
-        chalk.black.bgGreen(id),
-        result);
+async function printList(valida, result, id = '') {
+    if(valida) {
+        console.log(
+            chalk.yellow('lista validada'), 
+            chalk.black.bgGreen(id),
+            await validList(result))      
+    } else {
+        console.log(
+            chalk.yellow('lista de links'), 
+            chalk.black.bgGreen(id),
+            result);
+    }
 }
 
 async function textProcess(args) {
     const path = args[2];
+    const valida = args[3] === '--valida';
 
     try {
         fs.lstatSync(path);
@@ -25,12 +35,12 @@ async function textProcess(args) {
     }
     if(fs.lstatSync(path).isFile()) {
         const resultado = await getFile(args[2]);
-        printList(resultado);
+        printList(valida, resultado);
     } else if (fs.lstatSync(path).isDirectory()) {
         const files = await fs.promises.readdir(path)
         files.forEach(async (fileName) => {
             const list = await getFile(`${path}/${fileName}`)
-            printList(list, fileName);
+            printList(valida, list, fileName);
         })
     }
     
